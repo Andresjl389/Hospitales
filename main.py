3e0f2d71cd7dfa17b701e339cf8dd8f5b81c0ea0
Andresjl389 <andresjaimes195@gmail.com>
2025-11-14 17:57:29 -0500

    feat: add origins to cors

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app.mount("/media", StaticFiles(directory="media"), name="media")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['https://hospitales-frontend.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
